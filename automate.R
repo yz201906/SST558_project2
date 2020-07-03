26cd9b7bbc7eb9fa874255699849f4cd05886df2
YINZHOU ZHU <yinzhou.zhu@duke.edu>
2020-07-02 20:53:52 -0400

    Delete automate.R

--- automate.R
+++
@@ -1,9 +0,0 @@
-library(rmarkdown)
-library(tidyverse)
-
-days <- c("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday")
-
-map(.x = days, .f = ~render(input = "README.Rmd", 
-                            output_file = paste0(.x,"report.html"), 
-                            params = list(day_of_week = .x))
-    )
